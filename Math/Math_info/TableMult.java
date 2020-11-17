f1cdb85b8aac07c266e954ab2f1cbc047b2a9a5f
laruchsix <thib.ruch@laposte.net>
2020-11-17 11:23:33 +0100

    modifié :         Math_info/TableMult.class 	modifié :         Math_info/TableMult.java

--- Math/Math_info/TableMult.java
+++ Math/Math_info/TableMult.java
@@ -14,15 +14,51 @@ public class TableMult {
 	public void initTableau(){
 		for( int i=0; i< this.tableau.length ; i++){
 			for( int j = 0 ; j< this.tableau.length ; j++){
-				this.tableau[i][j] = (i*j);
+				this.tableau[i][j] = ((i+1)*(1+j));
 			}
 		}
 	}
 	
+	public void printNumber(int current){
+		if(current < 10 )
+				System.out.print("  " + current + "  ");
+			else{
+				if(current < 100 )
+					System.out.print(" " + current + "  ");
+				else{
+					if(current < 1000 )
+						System.out.print(" " + current + " ");
+				}
+			}
+	}
+
+
+	public void printline(int i){
+		for(int current : this.tableau[i]){
+			printNumber(current);
+		}
+	}
+
+	public void printTableau(){
+		System.out.print("     ");
+		for(int i=1; i <= this.tableau.length; i++){
+			printNumber(i);
+		}
+
+		System.out.println();
+		for(int i=0; i< this.tableau.length; i++){
+			int [] current = this.tableau[i];
+			printNumber(i+1);
+			printline(i);
+			System.out.println();
+		}
+
+	}
 	
 
 
 	public static void main(String[] args){
-		
+		TableMult tab = new TableMult(15);
+		tab.printTableau();
   }
 }
